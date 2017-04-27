065704c52d73a118220377509be8b7bde04c9690
Loïck Virot <virot.loick@gmail.com>
2017-04-27 15:15:07 +0200

    factorise addLine

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var app = new Vue({
     focus: function () {
      this.$refs.userInput.focus();
    },
-    addLine: function (message, command) {
+    addLine: function (message, command = false) {
       this.allMessages.push({value: message, command: command});
     },
     submit: function (e) {
@@ -19,44 +19,44 @@ var app = new Vue({
       var message = this.message.split(" ");
       switch (message[0]) {
         case "help":
-          this.addLine("clear : clear terminal", false);
-          this.addLine("ls : list elements in folder", false);
-          this.addLine("screenfetch : show info about vue-terminal", false);
+          this.addLine("clear : clear terminal");
+          this.addLine("ls : list elements in folder");
+          this.addLine("screenfetch : show info about vue-terminal");
           break;
         case "ls":
-          this.addLine("about work contact", false);
+          this.addLine("about work contact");
           break;
         case "clear":
           this.allMessages = [];
           break;
         case "screenfetch":
-          this.addLine("=============================================", false);
-          this.addLine("VueJS terminal - Loïck Virot", false);
-          this.addLine("=============================================", false);
-          this.addLine("Available on github ! http://github.com/LoickVirot/vue-terminal", false);
-          this.addLine("Type help to list available commands", false);
+          this.addLine("=============================================");
+          this.addLine("VueJS terminal - Loïck Virot");
+          this.addLine("=============================================");
+          this.addLine("Available on github ! http://github.com/LoickVirot/vue-terminal");
+          this.addLine("Type help to list available commands");
           break;
         case "cat":
           if (message[1] !== undefined) {
             switch(message[1]) {
               case "about":
-                this.addLine("about info", false);
+                this.addLine("about info");
                 break;
               case "work":
-                this.addLine("work info", false);
+                this.addLine("work info");
                 break;
               case "contact":
-                this.addLine("contact info", false);
+                this.addLine("contact info");
                 break;
               default:
-                this.addLine("\"" + message[1] + "\" not found", false);
+                this.addLine("\"" + message[1] + "\" not found");
             }
             break;
           }
-          this.addLine("\"cat\" command require one parameter", false);
+          this.addLine("\"cat\" command require one parameter");
           break;
         default:
-          this.addLine("\"" + message[0] + "\" command not found. Type help to list available commands", false);
+          this.addLine("\"" + message[0] + "\" command not found. Type help to list available commands");
       }
       this.message = '';
     }
