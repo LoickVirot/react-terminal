5bc4a5d925b83c08884f5aae88a48a1603d86628
Loïck Virot <virot.loick@gmail.com>
2017-04-27 15:12:46 +0200

    commands can have parameters

--- js/app.js
+++ js/app.js
@@ -16,28 +16,47 @@ var app = new Vue({
       this.userInputProcess();
     },
     userInputProcess: function () {
-      switch (this.message) {
+      var message = this.message.split(" ");
+      switch (message[0]) {
         case "help":
           this.addLine("clear : clear terminal", false);
           this.addLine("ls : list elements in folder", false);
           this.addLine("screenfetch : show info about vue-terminal", false);
           break;
         case "ls":
-          this.addLine("About Work Contact", false);
+          this.addLine("about work contact", false);
           break;
         case "clear":
           this.allMessages = [];
           break;
-        case "screenfetch": {
+        case "screenfetch":
           this.addLine("=============================================", false);
           this.addLine("VueJS terminal - Loïck Virot", false);
           this.addLine("=============================================", false);
           this.addLine("Available on github ! http://github.com/LoickVirot/vue-terminal", false);
           this.addLine("Type help to list available commands", false);
           break;
-        }
+        case "cat":
+          if (message[1] !== undefined) {
+            switch(message[1]) {
+              case "about":
+                this.addLine("about info", false);
+                break;
+              case "work":
+                this.addLine("work info", false);
+                break;
+              case "contact":
+                this.addLine("contact info", false);
+                break;
+              default:
+                this.addLine("\"" + message[1] + "\" not found", false);
+            }
+            break;
+          }
+          this.addLine("\"cat\" command require one parameter", false);
+          break;
         default:
-          this.addLine(this.message + ": not found. Type help to list available commands", false);
+          this.addLine("\"" + message[0] + "\" command not found. Type help to list available commands", false);
       }
       this.message = '';
     }
